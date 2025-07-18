59f6fff42a564ce1db1fa2c088cbddd2c90304df
정회민 <wony1102@naver.com>
2025-07-18 23:44:38 +0900

    feat: DetailCardLayout 추가

---
+++ src/layout/DetailCardLayout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  summary: string;
+  body: Array<string>;
+}>();
+</script>
+
+<template>
+  <div class="detail-card">
+    <p class="summary caption">{{ summary }}</p>
+    <ul>
+      <li v-for="text of body">
+        <p class="text">{{ text }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.detail-card {
+  min-width: 20rem;
+  margin-inline: 1rem;
+  .text {
+    margin: 0;
+    text-align: start;
+    font-size: 0.97rem;
+  }
+}
+</style>
