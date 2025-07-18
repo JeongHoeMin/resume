b87081d4eb110fe5e2f42320684f175981ec1f82
정회민 <wony1102@naver.com>
2025-07-19 02:17:53 +0900

    feat: AboutMe 컴포넌트 추가

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <i class="profile"></i>
+</template>
+
+<style scoped>
+.profile {
+  width: 12rem;
+  height: 12rem;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center center;
+}
+</style>
---
+++ src/service/about-me.service.ts
@@ -0,0 +1,4 @@
+export const getAboutMe = () => {
+  return '' +
+    ''
+}
