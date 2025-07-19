a1b1e631408fba900be385c18bb6d8ddc5d948e7
정회민 <wony1102@naver.com>
2025-07-19 14:22:41 +0900

    feat: ProfileHeader 컴포넌트 추가 (fix: ProfileImage 컴포넌트에 사진 등록)

--- src/components/ProfileImage.vue
+++ src/components/ProfileImage.vue
@@ -3,15 +3,33 @@
 </script>
 
 <template>
-  <i class="profile"></i>
+  <div class="frame">
+    <i class="profile-img"></i>
+  </div>
 </template>
 
 <style scoped>
-.profile {
-  width: 12rem;
-  height: 12rem;
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center center;
+.frame {
+  scale: 1.1;
+  width: 10rem; /* 원하는 크기로 조절 */
+  height: 10rem;
+  border-radius: 50%; /* 동그랗게 만듦 */
+  overflow: hidden; /* 사진이 틀 밖으로 나가지 않도록 */
+  box-shadow: 0 0 1rem rgba(0,0,0,0.8); /* 약간의 그림자 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white; /* 액자 배경 */
+
+  .profile-img {
+    background-image: url("../assets/hoeminJ.jpg");
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    object-fit: cover;
+    background-size: contain;
+    scale: 1.5;
+    background-position: 16px 6px;
+  }
 }
 </style>
---
+++ src/service/profile.service.ts
@@ -0,0 +1,12 @@
+export const getName = () => {
+  return ''
+}
+export const getPosition = () => {
+  return ''
+}
+export const getEmail = () => {
+  return ''
+}
+export const getGithubLink = () => {
+  return ''
+}
