57182ae2a8b475efc762e5b39f317699ac709946
정회민 <wony1102@naver.com>
2025-07-20 21:01:39 +0900

    feat: Skill 컴포넌트 추가

---
+++ src/components/SkillCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import DetailCardLayout, {type IDetailCardProps} from "../layout/DetailCardLayout.vue";
+import RecapCardLayout, {type IRecapCardProps} from "../layout/RecapCardLayout.vue";
+
+export interface ISkillCardProps extends IRecapCardProps, IDetailCardProps {}
+
+defineProps<ISkillCardProps>();
+</script>
+
+<template>
+  <div class="card">
+    <RecapCardLayout
+        :title="title"
+    />
+    <DetailCardLayout
+        :body="body"
+    />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 100%;
+  display: flex;
+  margin: 1rem 0 0;
+  padding: 0;
+}
+
+@media (max-width: 42rem) {
+  .card {
+    flex-direction: column;
+  }
+}
+</style>
---
+++ src/components/SkillSection.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import CardLayout from "../layout/CardLayout.vue";
+import SkillCard, {type ISkillCardProps} from "./SkillCard.vue";
+import {onBeforeMount, ref} from "vue";
+import {getSkillList} from "../service/skill.service.ts";
+
+const skillList = ref<ISkillCardProps[]>();
+
+onBeforeMount(() => {
+  skillList.value = getSkillList();
+})
+</script>
+
+<template>
+  <card-layout header="기술 스택" hint="지금까지 배포해본 서비스들에서 사용했던 기술들입니다.">
+    <SkillCard
+        v-for="({title, body}, idx) in skillList"
+        :key="idx"
+        :title="title"
+        :body="body"
+    />
+  </card-layout>
+</template>
+
+<style scoped>
+
+</style>
